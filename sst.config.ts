2fb4cbd7515e0227113e1d203237b50935be4510
offwork <chomunist@gmail.com>
2024-11-23 02:52:40 +0300

    sst aws astro

---
+++ sst.config.ts
@@ -0,0 +1,17 @@
+/// <reference path="./.sst/platform/config.d.ts" />
+
+export default $config({
+  app(input) {
+    return {
+      name: "deveroppers",
+      removal: input?.stage === "production" ? "retain" : "remove",
+      home: "aws",
+    };
+  },
+  async run() {
+    const bucket = new sst.aws.Bucket("MyBucket", {
+      access: "public",
+    });
+    new sst.aws.Astro("MyWeb", { link: [bucket] });
+  },
+});
